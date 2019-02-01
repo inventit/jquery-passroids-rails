b85ef822b7aa21371c9855efa6964095b234f271
Toshiyuki HINA <thina@yourinventit.com>
2019-02-01 10:38:27 +0900

    initial commit

---
+++ lib/jquery/passroids/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/passroids/rails/version"
+
+module Jquery
+  module Passroids
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
