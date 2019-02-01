b237275d4b7de39f1b01f449a304e2286c02030a
Toshiyuki HINA <thina@yourinventit.com>
2019-02-01 14:43:40 +0900

    add test spec

--- spec/passroids_spec.rb
+++ spec/passroids_spec.rb
@@ -2,7 +2,7 @@ require 'passroids'
 
 RSpec.describe Passroids do
   describe "#strength" do
-    context "Password length: < 4" do
+    context "password length < 4" do
       it "should return 'Weak' if password is nil" do
         expect(Passroids.strength(nil)).to eq("Weak")
       end
@@ -12,21 +12,65 @@ RSpec.describe Passroids do
       end
     end
 
-    context "Password length: 4 <= and < 5" do
-      it "should reutrn 'Weak' if all letters are lower case" do
-        expect(Passroids.strength('a' * 4)).to eq("Weak")
+    context "password length == 4" do
+      context "and all letters are lower case" do
+        it "should return 'Weak'" do
+          expect(Passroids.strength('a' * 4)).to eq("Weak")
+        end
+      end
+
+      context "and at least one upper case letter" do
+        it "should return 'Weak'" do
+          expect(Passroids.strength('aaAa')).to eq("Weak")
+        end
+
+        context "and at least one number" do
+          it "should return 'Medium'" do
+            expect(Passroids.strength('aaA1')).to eq("Medium")
+          end
+        end
+
+        context "and at least three numbers" do
+          it "should return 'Medium'" do
+            expect(Passroids.strength('11A1')).to eq("Medium")
+          end
+        end
+
+        context "and at least one special character" do
+          it "should return 'Medium'" do
+            expect(Passroids.strength('aA!a')).to eq("Medium")
+          end
+        end
+
+        context "and at least two special characters" do
+          it "should return 'Strong'" do
+            expect(Passroids.strength('aA!!')).to eq("Strong")
+          end
+        end
+
+        context "and both upper and lower case" do
+          it "should return 'Medium'" do
+            expect(Passroids.strength('aAaA')).to eq("Weak")
+          end
+        end
+
+        context "and both letters and numbers" do
+          it "should return 'Medium'" do
+            expect(Passroids.strength('aAa1')).to eq("Medium")
+          end
+        end
       end
     end
 
-    context "Password length: 5 <= and < 8" do
+    context "4 <= password length < 8" do
       it "must be tested"
     end
 
-    context "Password length: 8 <= and < 16" do
+    context "8 <= password length < 16" do
       it "must be tested"
     end
 
-    context "Password length: 15 <=" do
+    context "16 <= password length" do
       it "must be tested"
     end
   end
