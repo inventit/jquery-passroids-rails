4008a6e3deafaaa0814c6258ec6f96fac82260a5
Toshiyuki HINA <thina@yourinventit.com>
2019-02-01 13:03:39 +0900

    add some tests

--- spec/passroids_spec.rb
+++ spec/passroids_spec.rb
@@ -1,7 +1,33 @@
 require 'passroids'
 
 RSpec.describe Passroids do
-  it "#strength should return 'weak' with a password less than 4 characters long" do
-    expect(Passroids.strength('a' * 3)).to eq('Weak')
+  describe "#strength" do
+    context "Password length: < 4" do
+      it "should return 'Weak' if password is nil" do
+        expect(Passroids.strength(nil)).to eq("Weak")
+      end
+
+      it "should return 'Weak' if password length < 4" do
+        expect(Passroids.strength('a' * 3)).to eq("Weak")
+      end
+    end
+
+    context "Password length: 4 <= and < 5" do
+      it "should reutrn 'Weak' if all letters are lower case" do
+        expect(Passroids.strength('a' * 4)).to eq("Weak")
+      end
+    end
+
+    context "Password length: 5 <= and < 8" do
+      it "must be tested"
+    end
+
+    context "Password length: 8 <= and < 16" do
+      it "must be tested"
+    end
+
+    context "Password length: 15 <=" do
+      it "must be tested"
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require "rails/engine"
 require "jquery/passroids/rails"
 
 RSpec.configure do |config|
